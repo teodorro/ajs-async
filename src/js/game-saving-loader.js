211bdcab243309e3965b8bc02d6dca29b8218537
theo <edem.teo@gmail.com>
2023-07-31 22:54:51 +0300

    tests

--- src/js/game-saving-loader.js
+++ src/js/game-saving-loader.js
@@ -6,19 +6,15 @@ export default class GameSavingLoader {
   static async load() {
     const binData = await read();
     const jsonData = await json(binData);
-    return new Promise((resolve) => {
-      const gsJson = JSON.parse(jsonData);
-      const gameSaving = new GameSaving(
-        gsJson.id,
-        gsJson.created,
-        gsJson.userInfo.id,
-        gsJson.userInfo.name,
-        gsJson.userInfo.level,
-        gsJson.userInfo.points,
-      );
-      resolve(gameSaving);
-    }).catch((error) => {
-      console.log(error);
-    });
+    const gsJson = JSON.parse(jsonData);
+    const gameSaving = new GameSaving(
+      gsJson.id,
+      gsJson.created,
+      gsJson.userInfo.id,
+      gsJson.userInfo.name,
+      gsJson.userInfo.level,
+      gsJson.userInfo.points,
+    );
+    return gameSaving;
   }
 }
